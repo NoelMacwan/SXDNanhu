e6f8df33e299c8e1dba5179378b61f64befc1a2c
NoelMacwan <yulespencer@gmail.com>
2014-04-10 12:08:02 +0530

    revision: cpufreq: add smooth scrolling table

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -87,6 +87,8 @@ static void set_cpu_work(struct work_struct *work)
 
 unsigned int smooth_level = 99;
 
+unsigned int smooth_level = 99;
+
 static int msm_cpufreq_target(struct cpufreq_policy *policy,
 				unsigned int target_freq,
 				unsigned int relation)
@@ -94,7 +96,10 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 	int ret = -EFAULT;
 	int index;
 	struct cpufreq_frequency_table *table;
+<<<<<<< HEAD
 #ifdef CONFIG_SMP
+=======
+>>>>>>> c2dab9d... Added Smooth Scrolling CPU Freq Table
 	int i, old_index = INT_MAX;
 	static int print_err = 0; /*KERNEL-SC-cpu-frequency-02+*/
 
@@ -131,7 +136,28 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 	pr_debug("CPU[%d] target %d relation %d (%d-%d) selected %d\n",
 		policy->cpu, target_freq, relation,
 		policy->min, policy->max, table[index].frequency);
+<<<<<<< HEAD
 #endif
+=======
+//from galaxy s2...
+	if(smooth_level < 12)
+	{
+		for (i = 0; table[i].frequency != CPUFREQ_TABLE_END; i++) {
+			if (table[i].frequency == policy->cur)
+				old_index = table[i].index;
+		}
+		if (old_index != INT_MAX) {
+			//pr_debug("old_index: %d index: %d smooth_level: %d\n", old_index, index, smooth_level);
+			/* Do NOT step up max arm clock directly to reduce power consumption */
+			if (index > smooth_level && old_index < smooth_level)
+			{
+				index = smooth_level;
+				//pr_debug("Smooth level is set to %d\n", smooth_level);
+			}
+		}
+		//else pr_debug("old index not found\n");
+	}
+>>>>>>> c2dab9d... Added Smooth Scrolling CPU Freq Table
 
 //from galaxy s2...
 	if(smooth_level < 12)
